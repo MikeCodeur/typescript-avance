e3609d50d3e967ddb3d4378adf1c507897bf28de
Mike Codeur <cabanas.mickael@gmail.com>
2022-10-22 14:30:20 +0800

    exo5

---
+++ src/final/05.tsx
@@ -0,0 +1,25 @@
+// Overloads
+// http://localhost:3000/alone/final/02.ts
+
+// ❌ NE PAS MODIFIER
+import displayText, {init} from './helper/exerciseHelper'
+init()
+
+function printBirthDay(timestamp: Date): string
+function printBirthDay(timestamp: string): string
+function printBirthDay(inputDate: string | Date): string {
+  if (inputDate instanceof Date) {
+    return inputDate.toLocaleDateString()
+  } else if (typeof inputDate === 'string') {
+    return new Date(inputDate).toLocaleDateString()
+  } else {
+    return 'Non défini'
+  }
+}
+
+displayText(`${printBirthDay('October 13, 2014')}`)
+displayText(`${printBirthDay(new Date(2014, 9, 13))}`)
+
+/*eslint
+  @typescript-eslint/no-unused-vars: "off"
+*/
